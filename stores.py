0ba30b6cc4f27c526fc59ac9544c968609005c6c
Jerry Jeyachandra <jerrold.jeyachandra@gmail.com>
2022-03-16 13:29:01 -0400

    add initial draft for light store interface
    use store module in place of directly using Variable for testability

---
+++ stores.py
@@ -0,0 +1,92 @@
+'''
+Back-end stores for tracking persistent active job state
+for DRMAA Executor
+'''
+
+from typing import TYPE_CHECKING, Dict, Optional
+from typing_extensions import NotRequired, TypedDict
+
+from abc import abstractmethod, ABC
+
+from airflow.models import Variable
+from airflow.utils.session import provide_session
+from airflow.utils.log.logging_mixin import LoggingMixin
+
+if TYPE_CHECKING:
+    from sqlalchemy.orm import Session
+
+_TaskInstanceKeyDict = TypedDict(
+    '_TaskInstanceKeyDict', {
+        'dag_id': str,
+        'task_id': str,
+        'run_id': NotRequired[Optional[str]],
+        'execution_date': NotRequired[Optional[str]],
+        'try_number': int
+    })
+
+JobID = int
+JobTrackingType = Dict[JobID, _TaskInstanceKeyDict]
+
+
+class DRMAAJobStoreMixin(ABC):
+    '''
+    Interface for defining a store
+    backend for keeping track of DRMAA jobs
+    persistently across sessions
+    '''
+    @abstractmethod
+    def update(self, jobs: JobTrackingType, session: Session = None):
+        raise NotImplementedError
+
+    @abstractmethod
+    def get_or_create(self, session: Session = None):
+        raise NotImplementedError
+
+
+# TODO: Do an airflow version check for behaviour implementation
+class VariableStore(DRMAAJobStoreMixin, LoggingMixin):
+    '''
+    Use Airflow Variable as a back end job store
+    using a set key
+    '''
+    def __init__(self, metadata={}):
+        self.key: str = metadata.get('key', 'scheduler_job_ids')
+        self.description: str = metadata.get(
+            'description', 'DRMAA Scheduler Job ID tracking')
+        self.value = self.get_or_create()
+
+    @provide_session
+    def get_or_create(self, session: Session = None) -> JobTrackingType:
+        value = Variable.get(self.key, default_var=None, deserialize_json=True)
+        if value is None:
+            value = {}
+            self.log.info("Setting up job tracking Airflow variable...")
+
+            # Cannot use Variable.set in Airflow < 2.2
+            if session is not None:
+                session.add(
+                    Variable(
+                        key=self.key,
+                        val="{}",
+                        description=self.description,
+                    ))
+                session.flush()
+            self.log.info(f"Created `{self.key}` variable")
+
+        return value
+
+    @provide_session
+    def update(self, jobs: JobTrackingType, session: Session = None):
+        Variable.update(self.key, jobs, serialize_json=True, session=session)
+
+
+STORE_REGISTER = {"VariableStore": VariableStore}
+JobStoreType = DRMAAJobStoreMixin
+
+
+def get_store(store_name: str, metadata={}) -> JobStoreType:
+    '''
+    Retrieve a registered store and configure it
+    '''
+
+    return STORE_REGISTER[store_name](metadata)
