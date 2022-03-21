8b5bfda47ae3ba8d9b6fb9b0ca61d0696ec276ae
Jerry Jeyachandra <jerrold.jeyachandra@gmail.com>
2022-03-21 15:44:37 -0400

    wrap setup in __main__ check

--- setup.py
+++ setup.py
@@ -1,7 +1,8 @@
 from setuptools import setup
 
-setup(name="my-package",
-      entry_points={
-          "airflow.plugins":
-          ["drmaa_executors = drmaa_executor_plugin:DRMAAExecutorPlugin"]
-      })
+if __name__ == '__main__':
+    setup(name="drmaa_executor_plugin",
+          entry_points={
+              "airflow.plugins":
+              ["drmaa_executors = drmaa_executor_plugin:DRMAAExecutorPlugin"]
+          })
